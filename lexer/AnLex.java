ea9ea993ea3a03a2e61706e21b1233450450eaa1
Gustavo Scaglione Vilmar <gugascaglione2@gmail.com>
2023-06-01 22:37:36 -0300

    refactor: remove second Token ArrayList and its function calls within the method lex_evaluate - add package dependency token

--- lexer/AnLex.java
+++ lexer/AnLex.java
@@ -4,54 +4,43 @@
  */
 package lexer;
 import java.util.ArrayList;
-import token.Token;
+import token.*;
 
 public class AnLex {
-    
-    public ArrayList<Token> lex_evaluate (char[] expression) {
-        ArrayList<Token> token_List = new ArrayList<>(); // Array de Tokens da cadeia de entrada
-        ArrayList<Token> token_List_Output = new ArrayList<>(); // Array de Tokens de saída, considerando whitespaces
-                                                                // como Tokens
-        
+
+    //
+    public ArrayList<Token> lex_Evaluate(char[] expression) {
+        ArrayList<Token> token_List = new ArrayList<>();    // Array de Tokens da cadeia de entrada, CONSIDERANDO
+                                                            // whitespaces como Tokens
+
         for (char c : expression) { // 
-            if(Character.toString(c).matches("\\d")) {
-                token_List.add(Token.NUM);
-                token_List_Output.add(Token.NUM);
+            if (Character.toString(c).matches("\\d")) {
+                token_List.add(new Token(c, TokenType.NUM));
             } else if (Character.toString(c).matches("[+]|-|[*]|[//]")) {
-                token_List.add(Token.ARITHM_OPERATOR);
-                token_List_Output.add(Token.ARITHM_OPERATOR);
+                token_List.add(new Token(c, TokenType.ARITHM_OPERATOR));
             } else if (Character.toString(c).matches("[><]")) {
-                token_List.add(Token.LOGIC_OPERATOR);
-                token_List_Output.add(Token.LOGIC_OPERATOR);
+                token_List.add(new Token(c, TokenType.LOGIC_OPERATOR));
             } else if (Character.toString(c).matches("\\s")) {
-                // Ignorar whitespaces em token_List
-                token_List.add(Token.WHITESPACE);
-            } else if (Character.toString(c).matches("[A-Za-z,`@#$%^&|=]")) {
-                token_List.add(Token.CHAR);
-                token_List_Output.add(Token.CHAR);
+                token_List.add(new Token(c, TokenType.WHITESPACE));
+            } else if (Character.toString(c).matches("[A-Za-z,`!?@#$%^&|=]")) {
+                token_List.add(new Token(c, TokenType.CHAR));
             } else if (Character.toString(c).matches("\\(")) {
-                token_List.add(Token.PARENTHESIS_BEGIN);
-                token_List_Output.add(Token.PARENTHESIS_BEGIN);
+                token_List.add(new Token(c, TokenType.PARENTHESIS_BEGIN));
             } else if (Character.toString(c).matches("\\)")) {
-                token_List.add(Token.PARENTHESIS_END);
-                token_List_Output.add(Token.PARENTHESIS_END);
+                token_List.add(new Token(c, TokenType.PARENTHESIS_END));
             } else if (Character.toString(c).matches("\\{")) {
-                token_List.add(Token.SCOPE_BEGIN);
-                token_List_Output.add(Token.SCOPE_BEGIN);
+                token_List.add(new Token(c, TokenType.SCOPE_BEGIN));
             } else if (Character.toString(c).matches("}")) {
-                token_List.add(Token.SCOPE_END);
-                token_List_Output.add(Token.SCOPE_END);
+                token_List.add(new Token(c, TokenType.SCOPE_END));
             } else if (Character.toString(c).matches(";")) {
-                token_List.add(Token.END_INSTRUCTION);
-                token_List_Output.add(Token.END_INSTRUCTION);
+                token_List.add(new Token(c, TokenType.END_INSTRUCTION));
             } else {
-                token_List.add(Token.ERROR);
-                token_List_Output.add(Token.ERROR);
+                token_List.add(new Token(c, TokenType.ERROR));
             }
         }
 
-        token_List.add(Token.EOF); // Adição do Token "EOF" ao fim da lista de Tokens
-        token_List_Output.add(Token.EOF);
-        return token_List_Output;
+        token_List.add(new Token(TokenType.EOF)); // Adição do Token "EOF" ao fim da lista de Tokens
+
+        return lex_Evaluate_Strings(token_List);
     }
 }
